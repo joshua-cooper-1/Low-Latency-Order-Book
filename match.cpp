abd8a8a888d4282bff55ce07f21a6f79ffa78487
Joshua Cooper <coopsjoshua@gmail.com>
2026-08-09 13:50:17 +0100

    Added maxHeap class that inherits from parent Heap class

--- match.cpp
+++ match.cpp
@@ -170,6 +170,28 @@ class MinHeap : public Heap {
 };
 
 
+class MaxHeap : public Heap {
+    private:
+        
+        bool is_heap_condition_satisfied(int parent, int child) override {
+            if (heap[parent] > heap[child]) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        int choose_child(int leftChild, int rightChild) override {
+            if (heap[leftChild] > heap[rightChild]) {
+                return leftChild;
+            }
+            else {
+                return rightChild;
+            }
+        }
+};
+
 
 int main() {
     MinHeap minHeap = MinHeap();
