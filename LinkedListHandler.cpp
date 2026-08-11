bf847d951941604ae242080d3b11d087538c9555
Joshua Cooper <coopsjoshua@gmail.com>
2026-08-11 20:52:06 +0100

    @IM

--- LinkedListHandler.cpp
+++ LinkedListHandler.cpp
@@ -67,18 +67,94 @@ class LinkedListHandler {
             //binary search to find the price to remove (probs not needed)
         }
 
-        void addNodeL2(LinkedListNodeL2* L2NodeToAdd) {
+        LinkedListNodeL1* getNodeL1(int price, LinkedListNodeL1* current) {
+                        //binary search to find where to put in
+
+            if (current == nullptr) {
+                current = root;
+            }
+            
+
+     
+            
+           
+            if (price == current->getPrice()) {
+                return current;
+
+            }
+            else if (price < current->getPrice()) {
+                if (current->getLeftChildL1() != nullptr) {
+                    return getNodeL1(price, current->getLeftChildL1());
+                    
+                }
+                else {
+                    //doesnt exist
+                    return nullptr;
+
+                }
+            }
+            else if (price > current->getPrice()) {
+                if (current->getRightChildL1() != nullptr) {
+                    return getNodeL1(price, current->getRightChildL1());
+                    
+                }
+                else {
+                    //diesbnt exist
+                    return nullptr;
+
+                }
+            }
+        
+            
+        }
+        void addNodeL2(LinkedListNodeL2* NodeL2ToAdd) {
             //get the price somehow and then insert at the back of the queue?
+            int price = NodeL2ToAdd->getPrice();
+
+            LinkedListNodeL1* nodeL1Pointer = getNodeL1(price, root);
+
+            if (nodeL1Pointer == nullptr) {
+                LinkedListNodeL1 nodeL1;
+                nodeL1.setPrice(price);
+                nodeL1Pointer = &nodeL1;
+                addNodeL1(nodeL1Pointer, nullptr);
+            }
+
+            nodeL1Pointer->addNodeL2(NodeL2ToAdd);
+
+
         }
 
-        void removeNodeL2(LinkedListNodeL2* L2NodeToRemove) {
+        void removeNodeL2(int orderId) {
             //find index of where it is in L1 using separate tracker
             //find index of where it is in L2 using separate tracker
             //remove it
+            LinkedListNodeL2* nodeL2 = orderTracker[orderId];
+
+            LinkedListNodeL2* previousNodeL2 = nodeL2->getPrevNode();
+            LinkedListNodeL2* nextNodeL2 = nodeL2->getNextNode();
+
+            previousNodeL2->setNextNode(nextNodeL2);
+            nextNodeL2->setPrevNode(previousNodeL2);
+
+            //delete nodeL2?
+
+
         }
 
-        LinkedListNodeL1* getNextOrder(int price) {
-            //get the order at the front of the queue for this price?
+
+
+        LinkedListNodeL2* getNextOrder(int price) {
+            LinkedListNodeL1* priceNodeL1 = getNodeL1(price, nullptr);
+
+            if (priceNodeL1 == nullptr) {
+                return nullptr;
+            }
+
+            LinkedListNodeL2* nextOrderNodeL2 = priceNodeL1->getNextNodeL2();
+
+            return nextOrderNodeL2;
+
         }
 
         void rebalanceTree() {
--- LinkedListNodeL1.cpp
+++ LinkedListNodeL1.cpp
@@ -18,16 +18,16 @@ LinkedListNodeL1* LinkedListNodeL1::getRightChildL1() {
     return rightChildL1;
 }
 
-void LinkedListNodeL1::addNodeL2(LinkedListNodeL2* NodeL2) {
+void LinkedListNodeL1::addNodeL2(LinkedListNodeL2* NodeL2ToAdd) {
     
     if (headL2 == nullptr) {
-        this->headL2 = NodeL2;
+        this->headL2 = NodeL2ToAdd;
     }
     else {
-        NodeL2->setPrevNode(this->tailL2);
-        this->tailL2->setNextNode(NodeL2);
+        NodeL2ToAdd->setPrevNode(this->tailL2);
+        this->tailL2->setNextNode(NodeL2ToAdd);
     }
-    this->tailL2 = NodeL2;
+    this->tailL2 = NodeL2ToAdd;
     
 }
 
