8ec1a1fec568a001622e03a9ab23847b688312de
Joshua Cooper <coopsjoshua@gmail.com>
2026-09-09 10:51:50 +0100

    chore: remove deprecated heap-based order book prototype

--- MinHeap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Heap.h"
-
-class MinHeap : public Heap {
-    public:
-        bool is_heap_condition_satisfied(int parent, int child) override;
-        int choose_child(int leftChild, int rightChild) override;
-};
